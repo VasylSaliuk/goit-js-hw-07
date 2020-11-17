7e5e86a5dd8d0da6d66883f816ac548a9dedb538
Vasyl Saliuk <67233665+VasylSaliuk@users.noreply.github.com>
2020-11-18 00:04:37 +0200

    add 7-8 task

--- js/task-7-8.js
+++ js/task-7-8.js
@@ -1,8 +1,32 @@
-import users from './array.js';
+const input = document.querySelector('#controls > input');
+const renderBtn = document.querySelector('[data-action="render"]');
+const destroyBtn = document.querySelector('[data-action="destroy"]');
+const boxes = document.getElementById('boxes');
+let div = [];
 
-// Write code under this line
-const calculateTotalBalance = array => array.reduce((acc, {balance}) => acc + balance, 0);
+renderBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
 
-console.log(calculateTotalBalance(users)); 
-// 20916
+function createBoxes() {
+    const value = Number(input.value);
+    createDivs(value);
+};
 
+function createDivs(amount) {
+    let size = 30;
+    for (let i = 0; i < amount; i++) {
+        size += 10;
+        const createDiv = document.createElement('div');
+        createDiv.style.width = `${size}px`;
+        createDiv.style.height = `${size}px`;
+        createDiv.style.backgroundColor = `#${Math.random().toString(16).substr(-6)}`;
+        div.push(createDiv);
+    }
+    boxes.append(...div);
+};
+
+function destroyBoxes() {
+    div = [];
+    boxes.innerHTML = '';
+    input.value = '';
+};
